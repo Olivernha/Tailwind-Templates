946520f521802af4e71338bf7261254c2f8d8033
Nay Htet Aung <nayhtetaung201733@gmail.com>
2021-03-28 12:02:29 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        height: {
+            '112': '28rem'
+        },
+
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
